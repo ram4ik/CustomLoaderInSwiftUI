dba1d57255f0f9d51db144adfda8c6271904c747
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-14 15:48:59 +0300

    Initial Commit

---
+++ CustomLoaderInSwiftUI/CustomLoaderInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomLoaderInSwiftUIApp.swift
+//  CustomLoaderInSwiftUI
+//
+//  Created by ramil on 14.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct CustomLoaderInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
